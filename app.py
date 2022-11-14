cad79cb52f6cce864613249c95319c65cc04dbb5
adityatriand <adityatriananda0@gmail.com>
2022-11-15 00:14:42 +0700

    [FIX] fix for using msrcr and add some routing

--- app.py
+++ app.py
@@ -1,11 +1,53 @@
-from flask import Flask, render_template, Response, jsonify
+from flask import Flask, render_template, Response, request
 from camera import Video
+import cv2 as cv
 
 app = Flask(__name__)
+
+app.config['SEND_FILE_MAX_AGE_DEFAULT'] = 1
+faceDetect = cv.CascadeClassifier('support/haarcascade_frontalface_default.xml')
 
 @app.route('/')
 def index():
     return render_template('index.html')
+
+@app.route('/guideapp')
+def guideapp():
+    return render_template('guide.html')
+
+@app.route('/punishment')
+def punishment():
+    return render_template('punishment.html')
+
+@app.route('/tips')
+def tips():
+    return render_template('tips.html')
+
+@app.route('/detection', methods=['POST', 'GET'])
+def detection():
+    predict = None
+    if request.method == 'POST':
+        img = request.files['face']
+        img.save('static/file.jpg', 0)
+
+        image = cv.imread('static/file.jpg', cv.IMREAD_COLOR)
+        img_enh = Video().img_enh(image)
+        cv.imwrite('static/file.jpg', img_enh)
+
+        faces = faceDetect.detectMultiScale(img_enh, 1.3, 5)
+        print(len(faces))
+        if len(faces) != 0:
+            (x, y, w, h) = faces[0]
+            cropped = img_enh[y:y + h+30, x:x + w+30]
+            cv.imwrite('static/file.jpg', cropped)
+
+        final_image = cv.imread('static/file.jpg', cv.IMREAD_COLOR)
+        predict = Video().drowsiness_face(final_image)
+    return render_template('detection.html', predict=predict)
+
+@app.route('/detection/realtime')
+def realtime():
+    return render_template('realtime.html')
 
 def gen(camera):
     while True:
--- camera.py
+++ camera.py
@@ -2,6 +2,7 @@ import cv2 as cv
 import numpy as np
 from retinex import Retinex
 import dlib
+import pyttsx3
 from tensorflow.keras.models import load_model
 
 faceDetect = cv.CascadeClassifier('support/haarcascade_frontalface_default.xml')
@@ -18,36 +19,35 @@ class Video(object):
         is_dark = np.mean(img) < thrshld
         return True if is_dark else False
 
+    def img_enh(self, image):
+        is_image_dark = self.img_estim(image)
+        if is_image_dark:
+            img_enh = Retinex().msrcr(image)
+            return img_enh
+        else:
+            return image
+
     def extract_feat(self,image):
         detector = dlib.get_frontal_face_detector()
         predictor = dlib.shape_predictor('support/shape_predictor_68_face_landmarks.dat')
         shape = 0
-        # Detect the face
         rects = detector(image, 1)
-        # Detect landmarks for each face
+
         for rect in rects:
-            # Get the landmark points
             shape = predictor(image, rect)
-            # Convert it to the NumPy Array
             shape_np = np.zeros((68, 2), dtype="int")
             for i in range(0, 68):
                 shape_np[i] = (shape.part(i).x, shape.part(i).y)
             shape = shape_np
 
-            # Display the landmarks
             for i, (x, y) in enumerate(shape):
-                # Draw the circle to mark the keypoint
                 cv.circle(image, (x, y), 1, (0, 0, 255), -1)
         return image
 
     def drowsiness_face(self, image):
         IMG_SIZE = 145
-        is_image_dark = self.img_estim(image)
-        img_enh = image
-        if is_image_dark:
-            img_enh = Retinex().msrcr(image)
-        img_enh = self.extract_feat(img_enh)
-        fix_img = cv.resize(img_enh, (IMG_SIZE, IMG_SIZE), interpolation=cv.INTER_AREA)
+        img_feat = self.extract_feat(image)
+        fix_img = cv.resize(img_feat, (IMG_SIZE, IMG_SIZE), interpolation=cv.INTER_AREA)
         fix_img = fix_img / 255
         fix_img = fix_img.reshape(-1, 145, 145, 3)
         model = load_model('support/model.h5')
@@ -61,6 +61,7 @@ class Video(object):
         ret,frame = self.video.read()
         a = 30
         predict = 'no face'
+        frame = self.img_enh(frame)
         faces = faceDetect.detectMultiScale(frame, 1.3, 5)
         for x, y, w, h in faces:
             cv.rectangle(frame, (x, y), (x + w, y + h), (255, 0, 255), 2)
@@ -68,6 +69,11 @@ class Video(object):
                 (x, y, w, h) = faces[0]
                 cropped = frame[y:y + h+a, x:x + w+a]
                 predict = self.drowsiness_face(cropped)
+                engine = pyttsx3.init()
+                engine.setProperty('rate', 150)
+                if predict == 'drowsy':
+                    engine.say("You Are Drowsy")
+                    engine.runAndWait()
             cv.putText(frame, predict, (x, y - 10), cv.FONT_HERSHEY_SIMPLEX, 0.9, (255, 0, 255), 2)
         ret,jpg = cv.imencode('.jpg',frame)
         return jpg.tobytes()
